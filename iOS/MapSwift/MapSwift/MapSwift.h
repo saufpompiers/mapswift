1aee418c6d4a61666b9410fdfc0fdde784dcd886
David de Florinier <dave@deflorinier.me.uk>
2015-12-02 14:28:41 +0000

    initial commit

---
+++ iOS/MapSwift/MapSwift/MapSwift.h
@@ -0,0 +1,19 @@
+//
+//  MapSwift.h
+//  MapSwift
+//
+//  Created by David de Florinier on 02/12/2015.
+//  Copyright © 2015 Sauf Pompiers Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapSwift.
+FOUNDATION_EXPORT double MapSwiftVersionNumber;
+
+//! Project version string for MapSwift.
+FOUNDATION_EXPORT const unsigned char MapSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapSwift/PublicHeader.h>
+
+
