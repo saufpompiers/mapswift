ee115bbe4b07acd8bc9c08edf2e04a6b195f44d5
David de Florinier <dave@deflorinier.me.uk>
2016-01-04 16:55:02 +0000

    Match web colours, not configurable yet

---
+++ iOS/MapSwift/MapSwift/Utils/UIColorExtensions.swift
@@ -0,0 +1,42 @@
+//
+//  UIColorExtensions.swift
+//  MapSwift
+//
+//  Created by David de Florinier on 04/01/2016.
+//  Copyright © 2016 Sauf Pompiers Ltd. All rights reserved.
+//
+
+import Foundation
+
+//node background #E0E0E0
+//border #777
+//center node background #22AAE0
+//corner radius 10
+
+func mm_int2rgb(rgbValue: CUnsignedInt) -> (CGFloat, CGFloat, CGFloat) {
+    let div:CGFloat = 255.0
+    return (
+        CGFloat(UInt((rgbValue & 0xff0000) >> 16))/div,
+        CGFloat(UInt((rgbValue & 0xFF00) >> 8))/div,
+        CGFloat(UInt(rgbValue & 0xFF))/div
+    )
+}
+
+extension UIColor {
+
+    convenience init(hexValue: CUnsignedInt) {
+        let (r,g,b) = mm_int2rgb(hexValue)
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
+    convenience init(hexString: NSString) {
+        var hex = hexString as String
+        if hexString.hasPrefix("#") {
+            hex = hexString.substringWithRange(NSMakeRange(1, hexString.length - 1))
+        }
+        var rgbValue : CUnsignedInt = 0
+        NSScanner(string: hex).scanHexInt(&rgbValue)
+        let (r,g,b) = mm_int2rgb(rgbValue)
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
+
+}
--- iOS/MapSwift/MapSwift/Views/MapSwiftNodeView.swift
+++ iOS/MapSwift/MapSwift/Views/MapSwiftNodeView.swift
@@ -18,6 +18,11 @@ class MapSwiftNodeView: UIView {
             _node = n
             if let label = self.label, node = _node {
                 label.text = node.title
+                if node.level == 1 {
+                    self.backgroundColor = UIColor(hexString: "#22AAE0")
+                } else {
+                    self.backgroundColor = UIColor(hexString: "#E0E0E0")
+                }
             }
             self.setNeedsLayout()
         }
@@ -26,7 +31,7 @@ class MapSwiftNodeView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         let label = UILabel(frame:CGRectMake(0,0, frame.width, frame.height))
-        label.textColor = UIColor.grayColor()
+        label.textColor = UIColor(hexString: "#4F4F4F")
         label.numberOfLines = 0
         label.textAlignment = NSTextAlignment.Center
         label.font = UIFont.systemFontOfSize(16, weight: UIFontWeightSemibold)
@@ -35,6 +40,7 @@ class MapSwiftNodeView: UIView {
         self.label = label
         self.addSubview(label)
         self.defaultColors()
+
     }
     override func layoutSubviews() {
         if let label = self.label {
@@ -48,13 +54,13 @@ class MapSwiftNodeView: UIView {
 
     private func defaultColors() {
         self.backgroundColor = UIColor.lightTextColor();
-        self.layer.borderColor = UIColor.grayColor().CGColor;
+        self.layer.borderColor = UIColor(hexString: "#707070").CGColor
         self.layer.borderWidth = 1
         self.layer.cornerRadius = 10
-        self.layer.shadowColor = UIColor.lightGrayColor().CGColor;
+        self.layer.shadowColor = UIColor(hexString: "#070707").CGColor;
         self.layer.shadowOffset = CGSizeMake(1,1)
-        self.layer.shadowOpacity = 0.8
-        self.layer.shadowRadius = 1
+        self.layer.shadowOpacity = 0.4
+        self.layer.shadowRadius = 2
     }
     /*
     // Only override drawRect: if you perform custom drawing.
